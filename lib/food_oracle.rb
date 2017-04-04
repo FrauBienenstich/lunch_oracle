aaed9d35f6d8cd70e4191e537c4a14828a351f36
Alexander Lang <alex@upstre.am>
2017-04-04 11:06:35 +0200

    adds rspec

---
+++ lib/food_oracle.rb
@@ -0,0 +1,39 @@
+class FoodOracle
+  LUNCHES = ['BURGERZ', 'Anaveda', 'Cosmoveda', 'Fish House :fish:', 'Mercosy', ':hamburger: :hamburger:', 'Chez Michel :fr:', 'ottorink weinbar :wine_glass:', 'ORA', 'Lasan :halalparrot:'].freeze
+  BURGERS = ['Did you say burgers?', 'I heard Markthalle 9 has some good burgers.', 'A burger a day keeps the doctor away.', "Isn't it Burgersday today?", "Looks like it's the perfect weather for burgers.", 'What does the burger forecast say for today?'].freeze
+
+  def initialize(slack)
+    @slack = slack
+    @slack.on :hello do
+      puts "Successfully connected, welcome '#{@slack.self.name}' to the '#{@slack.team.name}' team at https://#{@slack.team.domain}.slack.com."
+    end
+
+    @slack.on :message do |data|
+      handle_message data
+    end
+
+    @slack.on :close do |_data|
+      puts 'Connection closing, exiting.'
+    end
+
+    @slack.on :closed do |_data|
+      puts 'Connection has been disconnected.'
+    end
+  end
+
+  def start
+    @slack.start!
+  end
+
+  private
+
+  def handle_message(data)
+    # puts data
+    case data.text
+    when /(!lunch)/
+      @slack.message channel: data.channel, text: LUNCHES.sample
+    when /(lunch)|(hungry)/
+      @slack.message channel: data.channel, text: BURGERS.sample
+    end
+  end
+end
